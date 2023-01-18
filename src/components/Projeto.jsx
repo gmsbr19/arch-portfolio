ba4af6b17bda491b178ce8cc1d285022dfeb9fc8
PORTELA Thiago <thiago.portela@vinci-airports.com.br>
2023-01-18 13:14:12 -0400

    improved img hover style and see more btn

--- src/components/Projeto.jsx
+++ src/components/Projeto.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 const Projeto = ({label, category, imgs, text}) => {
+    const [scaleImage, setScaleImage] = useState(false)
+
     return (
         <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-6 d-flex flex-column align-items-center">
-            <div className="w-100 h-100 overflow-hidden rounded" style={{height: "250px", maxHeight: "250px", minHeight: "250px"}}>
-                <img src={imgs[0]} className="img-fluid h-100 w-100" style={{objectFit: "cover"}} />
+            <div onMouseEnter={() => setScaleImage(true)} onMouseLeave={() => setScaleImage(false)} className={`w-100 h-100 overflow-hidden rounded position-relative d-flex align-items-center justify-content-center ${scaleImage ? "inner-shadow" : ""}`} style={{height: "250px", maxHeight: "250px", minHeight: "250px"}}>
+                <img src={imgs[0]} className="img-fluid h-100 w-100" style={{objectFit: "cover", transform: `${scaleImage ? "scale(1.1)" : ""}`}} />
+                <button className="see-more-btn position-absolute px-2">Ver mais</button>
             </div>
             <p className="mt-2">{label}</p>
         </div>
