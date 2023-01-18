3ab08a3b609e17b4f29066224c15bf751afe4e53
PORTELA Thiago <thiago.portela@vinci-airports.com.br>
2023-01-18 09:53:06 -0400

    main filter styles and hover effect on projects

--- src/components/Projeto.jsx
+++ src/components/Projeto.jsx
@@ -1,8 +1,8 @@
 const Projeto = ({label, category, imgs, text}) => {
     return (
         <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-6 d-flex flex-column align-items-center">
-            <div className="w-100 h-100" style={{height: "250px", maxHeight: "250px", minHeight: "250px"}}>
-                <img src={imgs[0]} className="rounded img-fluid h-100 w-100" style={{objectFit: "cover"}} />
+            <div className="w-100 h-100 overflow-hidden rounded" style={{height: "250px", maxHeight: "250px", minHeight: "250px"}}>
+                <img src={imgs[0]} className="img-fluid h-100 w-100" style={{objectFit: "cover"}} />
             </div>
             <p>{label}</p>
         </div>
--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -10,7 +10,24 @@ const Projetos = () => {
     console.log(projectsArr[1])
 
     return (
-        <div className="py-5 px-2 flex-grow-1">
+        <div className="py-5 px-2 flex-grow-1 d-flex flex-column gap-4">
+            <div className="row container w-100 gap-3 filter justify-content-center">
+                <button className="col-2">
+                    Arquitetônicos
+                </button>
+                <button className="col-2">
+                    Inst. Especiais
+                </button>
+                <button className="col-2">
+                    Paisagismo
+                </button>
+                <button className="col-2">
+                    Interiores
+                </button>
+                <button className="col-2">
+                    Reforma
+                </button>
+            </div>
             <div className="row container">
                 {projectsArr.map((el, i) => (
                     <Projeto key={i} label={el.label} category={el.category} imgs={el.imgs} text={el.text} />
